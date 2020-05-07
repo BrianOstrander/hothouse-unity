28014b2ceeadcc825d020e0fdc2a6b3128812ec8
Brian Ostrander <Ostrander.Brian@gmail.com>
2020-05-07 10:55:41 -0700

    Simplified desire handling.

--- Assets/Scripts/Ai/DwellerAgent/DwellerDesireState.cs
+++ Assets/Scripts/Ai/DwellerAgent/DwellerDesireState.cs
@@ -1,12 +1,34 @@
 using Lunra.Core;
 using Lunra.WildVacuum.Models;
 using Lunra.WildVacuum.Models.AgentModels;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace Lunra.WildVacuum.Ai
 {
 	public abstract class DwellerDesireState<S> : AgentState<GameModel, DwellerModel>
 		where S : DwellerDesireState<S>
 	{
+		protected DesireBuildingModel GetNearestDesireBuilding(
+			GameModel world,
+			DwellerModel agent,
+			out NavMeshPath path,
+			out Vector3 entrancePosition
+		)
+		{
+			return DwellerUtility.CalculateNearestEntrance(
+				agent.Position.Value,
+				world.DesireBuildings.AllActive,
+				b =>
+				{
+					if (b.DesireQuality.Value.TryGetValue(Desire, out var quality)) return 0f < quality;
+					return false;
+				},
+				out path,
+				out entrancePosition
+			);
+		}
+		
 		public override string Name => Desire + "Desire";
 
 		public abstract Desires Desire { get; }
@@ -16,8 +38,17 @@ namespace Lunra.WildVacuum.Ai
 		
 		public override void OnInitialize()
 		{
+			var timeoutState = new DwellerTimeoutState<S>();
+			
+			AddChildStates(
+				timeoutState,
+				new DwellerNavigateState<DwellerSleepDesireState>()
+			);
+			
 			AddTransitions(
 				new ToIdleOnDesireChanged(this as S),
+				new ToTimeoutForDesire(this as S, timeoutState),
+				new ToNavigateToNearestDesireBuilding(this as S),
 				new ToIdleOnShiftBegin(this as S)
 			);
 		}
@@ -63,6 +94,58 @@ namespace Lunra.WildVacuum.Ai
 			public ToIdleOnShiftBegin(S desireState) => this.desireState = desireState; 
 			
 			public override bool IsTriggered() => Agent.JobShift.Value.Contains(World.SimulationTime.Value);
+		}
+		
+		protected class ToTimeoutForDesire : AgentTransition<DwellerTimeoutState<S>, GameModel, DwellerModel>
+		{
+			S desireState;
+			DwellerTimeoutState<S> timeoutState;
+			BuildingModel target;
+
+			public ToTimeoutForDesire(
+				S desireState,
+				DwellerTimeoutState<S> timeoutState
+			)
+			{
+				this.desireState = desireState;
+				this.timeoutState = timeoutState;
+			}
+
+			public override bool IsTriggered()
+			{
+				target = desireState.GetNearestDesireBuilding(World, Agent, out _, out var entrancePosition);
+
+				if (target == null) return false;
+
+				return Mathf.Approximately(0f, Vector3.Distance(Agent.Position.Value.NewY(0f), entrancePosition.NewY(0f)));
+			}
+
+			public override void Transition()
+			{
+				timeoutState.ConfigureForNextTimeOfDay(Agent.JobShift.Value.Begin);
+				Agent.Desire.Value = Desires.None;
+			}
+		}
+		
+		protected class ToNavigateToNearestDesireBuilding : AgentTransition<DwellerNavigateState<DwellerSleepDesireState>, GameModel, DwellerModel>
+		{
+			S desireState;
+			DesireBuildingModel target;
+			NavMeshPath targetPath = new NavMeshPath();
+
+			public ToNavigateToNearestDesireBuilding(S desireState) => this.desireState = desireState;
+			
+			public override bool IsTriggered()
+			{
+				target = desireState.GetNearestDesireBuilding(World, Agent, out targetPath, out _);
+
+				return target != null;
+			}
+
+			public override void Transition()
+			{
+				Agent.NavigationPlan.Value = NavigationPlan.Navigating(targetPath);
+			}
 		}
 	}
 }
--- Assets/Scripts/Ai/DwellerAgent/DwellerDesireStates/DwellerSleepDesireState.cs
+++ Assets/Scripts/Ai/DwellerAgent/DwellerDesireStates/DwellerSleepDesireState.cs
@@ -1,92 +1,9 @@
-using System.Linq;
-using Lunra.Core;
 using Lunra.WildVacuum.Models;
-using Lunra.WildVacuum.Models.AgentModels;
-using UnityEngine;
-using UnityEngine.AI;
 
 namespace Lunra.WildVacuum.Ai
 {
 	public class DwellerSleepDesireState : DwellerDesireState<DwellerSleepDesireState>
 	{
-		static DesireBuildingModel GetNearestSleepBuilding(
-			GameModel world,
-			DwellerModel agent,
-			out NavMeshPath path,
-			out Vector3 entrancePosition
-		)
-		{
-			return DwellerUtility.CalculateNearestEntrance(
-				agent.Position.Value,
-				world.DesireBuildings.AllActive,
-				b =>
-				{
-					if (b.DesireQuality.Value.TryGetValue(Desires.Sleep, out var quality)) return 0f < quality;
-					return false;
-				},
-				out path,
-				out entrancePosition
-			);
-		}
-		
 		public override Desires Desire => Desires.Sleep;
-		
-		public override void OnInitialize()
-		{
-			var timeoutState = new DwellerTimeoutState<DwellerSleepDesireState>();
-			
-			AddChildStates(
-				timeoutState,
-				new DwellerNavigateState<DwellerSleepDesireState>()
-			);
-			
-			AddTransitions(
-				new ToIdleOnDesireChanged(this),
-				new ToTimeoutForSleep(timeoutState),
-				new ToNavigateToNearestSleepBuilding(),
-				new ToIdleOnShiftBegin(this)
-			);
-		}
-		
-		class ToTimeoutForSleep : AgentTransition<DwellerTimeoutState<DwellerSleepDesireState>, GameModel, DwellerModel>
-		{
-			DwellerTimeoutState<DwellerSleepDesireState> timeoutState;
-			BuildingModel target;
-
-			public ToTimeoutForSleep(DwellerTimeoutState<DwellerSleepDesireState> timeoutState) => this.timeoutState = timeoutState;
-			
-			public override bool IsTriggered()
-			{
-				target = GetNearestSleepBuilding(World, Agent, out _, out var entrancePosition);
-
-				if (target == null) return false;
-
-				return Mathf.Approximately(0f, Vector3.Distance(Agent.Position.Value.NewY(0f), entrancePosition.NewY(0f)));
-			}
-
-			public override void Transition()
-			{
-				timeoutState.ConfigureForNextTimeOfDay(Agent.JobShift.Value.Begin);
-				Agent.Desire.Value = Desires.None;
-			}
-		}
-		
-		class ToNavigateToNearestSleepBuilding : AgentTransition<DwellerNavigateState<DwellerSleepDesireState>, GameModel, DwellerModel>
-		{
-			DesireBuildingModel target;
-			NavMeshPath targetPath = new NavMeshPath();
-			
-			public override bool IsTriggered()
-			{
-				target = GetNearestSleepBuilding(World, Agent, out targetPath, out _);
-
-				return target != null;
-			}
-
-			public override void Transition()
-			{
-				Agent.NavigationPlan.Value = NavigationPlan.Navigating(targetPath);
-			}
-		}
 	}
 }
