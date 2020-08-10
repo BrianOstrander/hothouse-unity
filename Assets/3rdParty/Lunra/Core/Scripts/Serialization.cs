02352ca486ccf649ef5c9b165894fc9c95d5f4b2
Brian Ostrander <Ostrander.Brian@gmail.com>
2020-08-10 10:46:20 -0700

    Removed serializer that won't be used.

--- Assets/3rdParty/Lunra/Core/Scripts/Serialization.cs
+++ Assets/3rdParty/Lunra/Core/Scripts/Serialization.cs
@@ -38,7 +38,6 @@ namespace Lunra.Core
 					serializerSettings = new JsonSerializerSettings();
 					serializerSettings.ObjectCreationHandling = ObjectCreationHandling.Auto;
 					serializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
-					serializerSettings.ContractResolver = new NonPublicPropertiesResolver();
 					foreach (var converter in converters) serializerSettings.Converters.Add(converter);
 					foreach (var converter in addedConverters) serializerSettings.Converters.Add(converter);
 				}
@@ -61,7 +60,6 @@ namespace Lunra.Core
 					verboseSerializerSettings.TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple;
 					verboseSerializerSettings.ObjectCreationHandling = ObjectCreationHandling.Auto;
 					verboseSerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
-					serializerSettings.ContractResolver = new NonPublicPropertiesResolver();
 					foreach (var converter in converters) verboseSerializerSettings.Converters.Add(converter);
 					foreach (var converter in addedConverters) verboseSerializerSettings.Converters.Add(converter);
 				}
