cdfa9793ae25effd3912dec3f262b77edff04800
Brian Ostrander <Ostrander.Brian@gmail.com>
2020-08-28 13:40:45 -0700

    Comments.

--- Assets/3rdParty/Lunra/Satchel/Scripts/Constants.cs
+++ Assets/3rdParty/Lunra/Satchel/Scripts/Constants.cs
@@ -6,6 +6,7 @@ namespace Lunra.Satchel
 	{
 		const string Prefix = "satchel.";
 		
+		// TODO: I think some of these should be just on the item object itself... 
 		public static readonly PropertyKey<int> InstanceCount = new PropertyKey<int>(Prefix + nameof(InstanceCount).ToSnakeCase());
 		public static readonly PropertyKey<bool> IgnoreCleanup = new PropertyKey<bool>(Prefix + nameof(IgnoreCleanup).ToSnakeCase());
 		public static readonly PropertyKey<bool> Destroyed = new PropertyKey<bool>(Prefix + nameof(Destroyed).ToSnakeCase());
