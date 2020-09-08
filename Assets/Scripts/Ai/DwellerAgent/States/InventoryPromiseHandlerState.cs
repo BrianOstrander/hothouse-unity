bdc20918835d0e91904601a681edf31f80e70cac
Brian Ostrander <Ostrander.Brian@gmail.com>
2020-09-08 16:29:01 -0700

    Progress on handler state.

--- Assets/Scripts/Ai/DwellerAgent/States/InventoryPromiseHandlerState.cs
+++ Assets/Scripts/Ai/DwellerAgent/States/InventoryPromiseHandlerState.cs
@@ -1,4 +1,6 @@
+using System;
 using Lunra.Hothouse.Models;
+using Lunra.Satchel;
 using UnityEngine;
 
 namespace Lunra.Hothouse.Ai.Dweller
@@ -6,6 +8,12 @@ namespace Lunra.Hothouse.Ai.Dweller
 	public class InventoryPromiseHandlerState<S> : AgentState<GameModel, DwellerModel>
 		where S : AgentState<GameModel, DwellerModel>
 	{
+		// class Cache
+		// {
+		// 	public Item PromiseItem;
+		// 	public Stack PromiseStack;
+		// }
+	
 		TimeoutState timeoutInstance;
 		
 		public override void OnInitialize()
@@ -22,7 +30,65 @@ namespace Lunra.Hothouse.Ai.Dweller
 
 		public override void Begin()
 		{
-			Debug.Log("lol begin here");
+			while (Agent.InventoryPromises.All.TryPeek(out var promise))
+			{
+				if (Agent.Inventory.Container.TryFindFirst(promise, out var promiseItem))
+				{
+					var type = promiseItem[Items.Keys.Shared.Type];
+				
+					if (type == Items.Values.Shared.Types.Transfer)
+					{
+						var state = promiseItem[Items.Keys.Transfer.LogisticState];
+				
+						if (state == Items.Values.Transfer.LogisticStates.Pickup)
+						{
+							if (OnBeginPickup(promiseItem)) break;
+						}
+						else if (state == Items.Values.Transfer.LogisticStates.Dropoff)
+						{
+							if (OnBeginDropoff(promiseItem)) break;
+						}
+						else Debug.LogError($"Unrecognized {Items.Keys.Transfer.LogisticState}: {state}");
+					}
+					else Debug.LogError($"Unrecognized {Items.Keys.Shared.Type}: {type}");
+				}
+				else Debug.LogError($"Cannot find item for promise with id {promise}");
+
+				Agent.InventoryPromises.Break();
+			}
+		}
+
+		bool OnBeginTryGetReservation(
+			Item promiseItem,
+			PropertyKey<long> reservationIdKey,
+			out Item reservationItem
+		)
+		{
+			var reservationId = promiseItem[reservationIdKey];
+			if (Game.Items.TryGet(reservationId, out reservationItem)) return true;
+
+			Debug.LogError($"Unable to find {reservationIdKey} of {reservationId} for item {promiseItem}");
+			return false;
+		}
+		
+		bool OnBeginPickup(
+			Item promiseItem
+		)
+		{
+			if (!OnBeginTryGetReservation(promiseItem, Items.Keys.Transfer.ReservationPickupId, out var reservationItem)) return false;
+			
+			Debug.LogError("reserved begin is: "+reservationItem);
+
+			return true;
+		}
+		
+		bool OnBeginDropoff(
+			Item promiseItem
+		)
+		{
+			if (!OnBeginTryGetReservation(promiseItem, Items.Keys.Transfer.ReservationDropoffId, out var reservationItem)) return false;
+
+			throw new NotImplementedException();
 		}
 
 		public class ToInventoryPromiseHandlerOnAvailable : AgentTransition<S, InventoryPromiseHandlerState<S>, GameModel, DwellerModel>
